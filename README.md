03a617e6b4ab923d8b6493d8e57138d748316b56
wandersonsangreal <sangreal.wanderson@hotmail.com>
2021-05-26 12:27:06 -0300

    Método de criação no servidor

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Arquitetura hexagonal com Golang
 
 ```docker-compose up -d```
+
+```go run main.go cli --help```
+```go run main.go cli -a=create -n="Product CLI" -p=25.0```
+```go run main.go http --help```
+```go run main.go http```
--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/codeedu/go-hexagonal/adapters/dto"
 	"github.com/codeedu/go-hexagonal/application"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
@@ -14,6 +15,10 @@ func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.
 	r.Handle("/product/{id}", n.With(
 		negroni.Wrap(getProduct(service)),
 	)).Methods("GET", "OPTIONS")
+
+	r.Handle("/product", n.With(
+		negroni.Wrap(createProduct(service)),
+	)).Methods("POST", "OPTIONS")
 
 }
 
@@ -37,6 +42,42 @@ func getProduct(service application.ProductServiceInterface) http.Handler {
 
 		if err != nil {
 			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+	})
+
+}
+
+func createProduct(service application.ProductServiceInterface) http.Handler {
+
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+
+		rw.Header().Set("Content-Type", "application/json")
+
+		var productDto dto.Product
+
+		err := json.NewDecoder(r.Body).Decode(&productDto)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write(jsonError(err.Error()))
+			return
+		}
+
+		product, err := service.Create(productDto.Name, productDto.Price)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write(jsonError(err.Error()))
+			return
+		}
+
+		err = json.NewEncoder(rw).Encode(product)
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write(jsonError(err.Error()))
 			return
 		}
 
